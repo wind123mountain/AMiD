86e6af322a4f6d7c2083c57ce4a5ed98d5fbb9b8
phongdq <phongdq25@gmail.com>
2026-05-11 07:50:32 +0000

    update

---
+++ run_1gpu.sh
@@ -0,0 +1,13 @@
+bash install.sh
+
+source .venv/bin/activate
+
+bash ./scripts/download_data.sh
+
+if [ ! -d "./processed_data/ultraInteract" ]; then
+    bash ./scripts/process_data_ultraInteract.sh
+fi
+
+bash ./scripts/amid/train_llama3.2_3B_it.sh
+bash ./scripts/amid/train_qwen2.5_0.5B.sh
+bash ./scripts/amid/train_qwen2.5_1.5B_it.sh
