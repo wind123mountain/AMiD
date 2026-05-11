b8e48bc9734aec3673218f1d967a15d1a204f593
phongdq <phongdq25@gmail.com>
2026-05-11 08:04:02 +0000

    update

--- run_1gpu.sh
+++ run_1gpu.sh
@@ -8,6 +8,6 @@ if [ ! -d "./processed_data/ultraInteract" ]; then
     bash ./scripts/process_data_ultraInteract.sh
 fi
 
-bash ./scripts/amid/train_llama3.2_3B_it.sh
-bash ./scripts/amid/train_qwen2.5_0.5B.sh
-bash ./scripts/amid/train_qwen2.5_1.5B_it.sh
+bash ./scripts/amid_1gpu/train_llama3.2_3B_it.sh
+bash ./scripts/amid_1gpu/train_qwen2.5_0.5B.sh
+bash ./scripts/amid_1gpu/train_qwen2.5_1.5B_it.sh
