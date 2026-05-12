b541d570715656af9b7d5e94acd237533bf80bd0
phongdq <phongdq25@gmail.com>
2026-05-12 15:48:17 +0000

    u

--- install.sh
+++ install.sh
@@ -16,9 +16,6 @@ export NCCL_DEBUG=""
 # pip install vllm
 # pip install fraction
 # pip install jsonlines
-conda create -n amid python=3.10 -y
-conda init bash
-source ~/.bashrc
-conda activate amid
+
 pip install uv
-uv sync --active
+uv sync 
