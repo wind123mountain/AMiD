87ab7a970a4e453f87d78f1d4f36bf2c0102e060
phongdq <phongdq25@gmail.com>
2026-05-15 18:11:42 +0000

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
-bash install.sh
+# bash install.sh
 
-source .venv/bin/activate
+# source .venv/bin/activate
 
-bash ./scripts/download_data.sh
+# bash ./scripts/download_data.sh
 
-if [ ! -d "./processed_data/ultraInteract" ]; then
-    bash ./scripts/process_data_ultraInteract.sh
-fi
+# if [ ! -d "./processed_data/ultraInteract" ]; then
+#     bash ./scripts/process_data_ultraInteract.sh
+# fi
 
 bash ./scripts/distillm-nnm/train_llama3.2_3B_it.sh
 bash ./scripts/distillm-nnm/train_qwen2.5_0.5B.sh
--- scripts/distillm-nnm/train_llama3.2_3B_it.sh
+++ scripts/distillm-nnm/train_llama3.2_3B_it.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-GPUS=(1)
+GPUS=(0 1)
 export CUDA_VISIBLE_DEVICES=$(IFS=,; echo "${GPUS[*]}")
 
 MASTER_ADDR=localhost
