6288836e1fa3403b8df0665dc7a30b422611aa53
phongdq <phongdq25@gmail.com>
2026-05-16 06:43:09 +0000

    update

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@
 #     bash ./scripts/process_data_ultraInteract.sh
 # fi
 
-bash ./scripts/distillm-nnm/train_qwen2.5_1.5B_it_1e.sh
+# bash ./scripts/distillm-nnm/train_qwen2.5_1.5B_it_1e.sh
 bash ./scripts/distillm-nnm/train_qwen2.5_1.5B_it_2e.sh
-bash ./scripts/distillm-nnm/train_qwen2.5_1.5B_it_3e.sh
+# bash ./scripts/distillm-nnm/train_qwen2.5_1.5B_it_3e.sh
 bash ./scripts/distillm-nnm/train_qwen2.5_1.5B_it_1e_2.sh
 bash ./scripts/distillm-nnm/train_qwen2.5_1.5B_it_2e_2.sh
