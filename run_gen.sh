c6e1cb9380365591d65c15ffe392ac43aee9b12d
phongdq <phongdq25@gmail.com>
2026-05-16 13:13:15 +0000

    update

--- run_gen.sh
+++ run_gen.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=2,3 bash scripts/Qwen3-8B.sh 2
+# CUDA_VISIBLE_DEVICES=2,3 bash scripts/Qwen3-8B.sh 2
 CUDA_VISIBLE_DEVICES=2,3 bash scripts/gemma-2-9b-it.sh 2
-CUDA_VISIBLE_DEVICES=2,3 bash scripts/Qwen3-1.7B.sh 2
+# CUDA_VISIBLE_DEVICES=2,3 bash scripts/Qwen3-1.7B.sh 2
 CUDA_VISIBLE_DEVICES=2,3 bash scripts/gemma-2-2b-it.sh 2
