3e7ff1db9c425b87f135a915380753555f74a350
phongdq <phongdq25@gmail.com>
2026-05-18 16:09:16 +0000

    update

--- tools/generate_vllm.py
+++ tools/generate_vllm.py
@@ -32,7 +32,7 @@ def worker_inference(gpu_id, model_path, data_chunk, prompts_chunk, temp_out_pat
         dtype="bfloat16",
         tensor_parallel_size=1,
         gpu_memory_utilization=0.85,
-        seed=42, max_num_seqs=512,
+        seed=42, max_num_seqs=64,
     )
 
     sampling_params = SamplingParams(
