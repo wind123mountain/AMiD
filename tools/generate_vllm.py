077ac5af53254cf5d32d47f016f0ff819119e49b
phongdq <phongdq25@gmail.com>
2026-05-18 16:25:07 +0000

    update

--- tools/generate_vllm.py
+++ tools/generate_vllm.py
@@ -42,20 +42,8 @@ def worker_inference(gpu_id, model_path, data_chunk, prompts_chunk, temp_out_pat
         skip_special_tokens=True
     )
 
-    print(f"[GPU {gpu_id}] 🔤 Tokenizing...")
-    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    from vllm import TokensPrompt
-
-    token_ids = tokenizer(
-        prompts_chunk, 
-        return_tensors=None,
-        add_special_tokens=False
-    )["input_ids"]
-
-    token_prompts = [TokensPrompt(prompt_token_ids=ids) for ids in token_ids]
-
     print(f"[GPU {gpu_id}] ⚙️ Đang generate...")
-    outputs = llm.generate(token_prompts, sampling_params)
+    outputs = llm.generate(prompts_chunk, sampling_params)
 
     with open(temp_out_path, 'w', encoding='utf-8') as f:
         for i, output in enumerate(outputs):
